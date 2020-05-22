ab207cedaaf6b9a6cb3ef314be492a532d3b4b73
stelladum <stella.dumencic@gmail.com>
2020-05-22 10:11:26 +0200

    Added role management to admin

--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,8 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/admin', function(){
     return 'you are admin';
 })->middleware(['auth', 'auth.admin']);
+
+
+Route::namespace('Admin')->prefix('admin')->middleware(['auth', 'auth.admin'])->name('admin.')->group(function(){
+    Route::resource('/users', 'UserController', ['except' => ['show', 'create', 'store']]);
+});
