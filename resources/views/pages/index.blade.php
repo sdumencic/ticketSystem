9939b69247be599d1de8c4f0e4c03dab1a043250
sdumencic <stella.dumencic@gmail.com>
2020-07-20 11:35:14 +0200

    changes in index.blade.php

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,5 +4,9 @@
 <div class = "jumbotron text-center">
     <h1>{{$title}}</h1>
     <p>This is a ticket system</p>
+    @if (Auth::user())
+        <p>For creating a ticket press "create ticket"</p>
+    @else
     <p><a class = "btn btn-primary" href="/login" role="button">Login</a> <a class="btn btn-success" href = "/register" role = "button">Register</a><p>
+    @endif
 @endsection
